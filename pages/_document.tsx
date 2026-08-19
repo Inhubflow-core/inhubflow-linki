af476183172f0e567e20823946fa2fdccc8fbb9d
InHubFlow <contact@inhubflow.online>
2026-08-19 18:08:39 -0300

    feat: sync Outfit font configuration between Linki and Chatwoot

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;420;440;460;500;520;600;620;700;800&family=IBM+Plex+Mono:wght@400;500&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800&family=IBM+Plex+Mono:wght@400;500&display=swap"
           rel="stylesheet"
         />
       </Head>
